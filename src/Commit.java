89a65aff498c82fcc924d8259ee1d7b553559115
BrukArgaw <bkargaw@berkeley.edu>
2015-07-21 08:58:42 -0700

    this is what i have thus far

--- src/Commit.java
+++ src/Commit.java
@@ -1,10 +1,20 @@
-package proj2;
-
-public class Commit {
-
-	public void Commit(){
-		boolean isSplitPoint;
-		String id;
+ import java.util.*; 
+	public class Commit {
+		// commit must track its parents files not copy them...
+		// if the file is 
 		
+
+		HashMap<String, String> fileToLcation; 
+		boolean isSplitPoint;   // true if it is a split point else false
+		String ID;              // contains the Id of the commit object
+		String Message;
+		String Time;           // string that keeps track of the commit time for this node
+		Commit prevCommit;
+		public Commit(){
+			fileToLcation =new HashMap<String, String>();
+			 Date time = new Date();
+		       // display time and date using toString()
+		      Time= time.toString();	
+		}
 	}
-}
+
